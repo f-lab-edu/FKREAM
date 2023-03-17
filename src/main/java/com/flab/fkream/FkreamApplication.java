add85b757dcfefb4a1fe63f310ed31a44d76f6a7
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-03-17 15:10:18 +0900

    first commit

---
+++ src/main/java/com/flab/fkream/FkreamApplication.java
@@ -0,0 +1,13 @@
+package com.flab.fkream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkreamApplication.class, args);
+	}
+
+}
