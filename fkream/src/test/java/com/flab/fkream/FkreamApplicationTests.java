33264c217ea6fa56494ebf933e9fe2e98455f5e9
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-03-17 15:08:10 +0900

    first commit

---
+++ fkream/src/test/java/com/flab/fkream/FkreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.fkream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FkreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
