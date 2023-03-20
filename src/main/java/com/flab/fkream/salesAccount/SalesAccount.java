012e413368bcdbc69059bc75c4883f09e0f67ffd
JooDukHan <bfinecpa1@naver.com>
2023-03-21 00:20:45 +0900

    SalesAccount 클래스 annotaion 추가

--- src/main/java/com/flab/fkream/salesAccount/SalesAccount.java
+++ src/main/java/com/flab/fkream/salesAccount/SalesAccount.java
@@ -1,25 +1,23 @@
 package com.flab.fkream.salesAccount;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
+@EqualsAndHashCode(of = "id")
+@ToString
 public class SalesAccount {
 
     private Long id;
     private final Users users;
-
     private final String bankName;
     private final String accountNumber;
     private final String accountHolder;
     private final LocalDateTime createdAt;
 
-    public SalesAccount(Users users, String bankName, String accountNumber, String accountHolder, LocalDateTime createdAt) {
-        this.users = users;
-        this.bankName = bankName;
-        this.accountNumber = accountNumber;
-        this.accountHolder = accountHolder;
-        this.createdAt = createdAt;
-    }
 }
