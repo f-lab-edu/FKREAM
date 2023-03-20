bb625712d6ed48a0b3b3be317e8b0cdfefd27b03
JooDukHan <bfinecpa1@naver.com>
2023-03-20 16:33:57 +0900

    style: 변수명 변경

--- src/main/java/com/flab/fkream/users/Users.java
+++ src/main/java/com/flab/fkream/users/Users.java
@@ -1,8 +1,6 @@
 package com.flab.fkream.users;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -15,32 +13,32 @@ public class Users {
     private final String password;
 
     private final String gender;
-    private final String phone_number;
+    private final String phoneNumber;
 
-    private final String profile_name;
+    private final String profileName;
     private final String name;
     private final String rank;
 
-    private final String profile_img_name;
+    private final String profileImgName;
 
-    private final String profile_img_url;
-    private final String profile_img_origin_name;
+    private final String profileImgUrl;
+    private final String profileImgOriginName;
 
-    private final LocalDateTime created_at;
-    private final LocalDateTime modified_at;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
 
-    public Users(String email, String password, String gender, String phone_number,String profile_name, String name, String rank, String profile_img_name, String img_url, String profile_img_origin_name, LocalDateTime create_at, LocalDateTime modified_at) {
+    public Users(String email, String password, String gender, String phoneNumber, String profileName, String name, String rank, String profileImgName, String profileImgUrl, String profileImgOriginName, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.email = email;
         this.password = password;
         this.gender = gender;
-        this.phone_number = phone_number;
-        this.profile_name = profile_name;
+        this.phoneNumber = phoneNumber;
+        this.profileName = profileName;
         this.name = name;
         this.rank = rank;
-        this.profile_img_name = profile_img_name;
-        this.profile_img_url = img_url;
-        this.profile_img_origin_name = profile_img_origin_name;
-        this.created_at = create_at;
-        this.modified_at = modified_at;
+        this.profileImgName = profileImgName;
+        this.profileImgUrl = profileImgUrl;
+        this.profileImgOriginName = profileImgOriginName;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
     }
 }
--- src/main/java/com/flab/fkream/users/UsersMapper.java
+++ src/main/java/com/flab/fkream/users/UsersMapper.java
@@ -7,9 +7,9 @@ public interface UsersMapper {
 
     String insert = "INSERT INTO USERS(email, password, gender, phone_number, profile_name, name, rank, " +
             "profile_img_name, profile_img_url, profile_img_origin_name, created_at, modified_at) " +
-            "Values ( #{users.email}, #{users.password} , #{users.gender}, #{users.phone_number}" +
-            ",#{users.profile_name}, #{users.name}, #{users.rank}, #{users.profile_img_name},#{users.profile_img_url}" +
-            ", #{users.profile_img_origin_name}, #{users.created_at}, #{users.modified_at})";
+            "Values ( #{users.email}, #{users.password} , #{users.gender}, #{users.phoneNumber}" +
+            ",#{users.profileName}, #{users.name}, #{users.rank}, #{users.profileImgName},#{users.profileImgUrl}" +
+            ", #{users.profileImgOriginName}, #{users.createdAt}, #{users.modifiedAt})";
 
     @Insert(insert)
     @Options(useGeneratedKeys = true, keyProperty = "id")
