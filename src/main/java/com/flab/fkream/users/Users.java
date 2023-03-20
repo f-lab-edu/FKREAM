88757afef94ff5a88ee29b7b62f52974334a1067
JooDukHan <bfinecpa1@naver.com>
2023-03-21 00:25:12 +0900

    Users 클래스 annotaion 추가, 필드 추가

--- src/main/java/com/flab/fkream/users/Users.java
+++ src/main/java/com/flab/fkream/users/Users.java
@@ -1,16 +1,26 @@
 package com.flab.fkream.users;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 
 @Getter
+@Builder
+@EqualsAndHashCode(of = "id")
+@ToString
 public class Users {
 
     private Long id;
     private final String email;
     private final String password;
+
+    private final boolean fourteenAgreement;
+    private final boolean adAgreement;
+    private final boolean personalAuthentication;
 
     private final String gender;
     private final String phoneNumber;
@@ -18,6 +28,8 @@ public class Users {
     private final String profileName;
     private final String name;
     private final String rank;
+
+
 
     private final String profileImgName;
 
@@ -27,18 +39,4 @@ public class Users {
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
-    public Users(String email, String password, String gender, String phoneNumber, String profileName, String name, String rank, String profileImgName, String profileImgUrl, String profileImgOriginName, LocalDateTime createdAt, LocalDateTime modifiedAt) {
-        this.email = email;
-        this.password = password;
-        this.gender = gender;
-        this.phoneNumber = phoneNumber;
-        this.profileName = profileName;
-        this.name = name;
-        this.rank = rank;
-        this.profileImgName = profileImgName;
-        this.profileImgUrl = profileImgUrl;
-        this.profileImgOriginName = profileImgOriginName;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
-    }
 }
