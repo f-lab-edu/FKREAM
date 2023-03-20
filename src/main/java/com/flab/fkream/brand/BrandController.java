9c994e44955a99284814158e26e2cc818adb1364
Chanhook <cksgur97@gmail.com>
2023-03-20 23:36:49 +0900

    BrandController addBrand 추가

--- src/main/java/com/flab/fkream/brand/BrandController.java
+++ src/main/java/com/flab/fkream/brand/BrandController.java
@@ -1,39 +1,22 @@
 package com.flab.fkream.brand;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/brand")
 public class BrandController {
-	private final BrandMapper brandMapper;
-
-	@Autowired
-	public BrandController(BrandMapper brandMapper) {
-		this.brandMapper = brandMapper;
-	}
+	private final BrandService brandService;
 
 	@PostMapping("")
-	public Brand post(@RequestBody Brand brand) {
-		brandMapper.insert(brand);
-		return brand;
+	public HttpStatus addBrand(@RequestBody Brand brandInfo) {
+		brandService.addBrand(brandInfo);
+		return HttpStatus.CREATED;
 	}
-
-	@GetMapping("/{brandName}")
-	public Brand findByBrandName(@PathVariable("brandName") String brandName) {
-		return brandMapper.getByBrandName(brandName);
-	}
-
-	@GetMapping("")
-	public List<Brand> getAll() {
-		return brandMapper.getAll();
-	}
-
 }
