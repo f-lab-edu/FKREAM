fbe4a613db0f505e9388723f14b6348b26a96a70
JooDukHan <bfinecpa1@naver.com>
2023-03-22 15:39:46 +0900

    SalesAccountMapper.xml 위치 변경

--- src/main/java/com/flab/fkream/salesAccount/SalesAccountController.java
+++ src/main/java/com/flab/fkream/salesAccount/SalesAccountController.java
@@ -2,10 +2,12 @@ package com.flab.fkream.salesAccount;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/salesAccounts")
 public class SalesAccountController {
 
 }
