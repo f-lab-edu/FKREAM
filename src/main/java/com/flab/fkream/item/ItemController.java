c12a989b5f7b193ef12d114ac0f5add50ab6e032
JooDukHan <bfinecpa1@naver.com>
2023-03-28 17:51:59 +0900

    controller 세팅
    필요한 controller 세팅

--- src/main/java/com/flab/fkream/item/ItemController.java
+++ src/main/java/com/flab/fkream/item/ItemController.java
@@ -3,11 +3,7 @@ package com.flab.fkream.item;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,19 +12,39 @@ import lombok.RequiredArgsConstructor;
 public class ItemController {
 	private final ItemService itemService;
 
-	@PostMapping("/item")
+	// item 추가
+	@PostMapping("/items")
 	public HttpStatus addItem(@RequestBody Item itemInfo) {
+
 		itemService.addItem(itemInfo);
 		return HttpStatus.CREATED;
 	}
 
+
+	// item 리스트 조회,
 	@GetMapping("/items")
 	public List<Item> findAll() {
 		return itemService.findAll();
 	}
 
-	@GetMapping("/item/{id}")
+
+	// item 단건 조회
+	@GetMapping("/items/{id}")
 	public Item findOne(@PathVariable Long id) {
 		return itemService.findOne(id);
 	}
+
+
+	// item 수정 Todo: 서비스 수정
+	@PatchMapping("/items/{id}")
+	public void update(@PathVariable Long id) {
+
+	}
+
+	// item 삭제
+	@DeleteMapping ("/items/{id}")
+	public void delete(@PathVariable Long id){
+
+	}
+
 }
