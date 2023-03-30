cdf772fea5ca1d282db8ad76e0dc2c105eee43f2
JooDukHan <bfinecpa1@naver.com>
2023-03-30 20:22:42 +0900

    ItemImg controller, service, mapper 단위 테스트 구현
    #14

---
+++ src/test/java/com/flab/fkream/itemImg/ItemImgMapperTest.java
@@ -0,0 +1,67 @@
+package com.flab.fkream.itemImg;
+
+import com.flab.fkream.item.Item;
+import com.flab.fkream.item.ItemMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ItemImgMapperTest {
+
+    @Autowired
+    ItemImgMapper itemImgMapper;
+    @Autowired
+    ItemMapper itemMapper;
+
+    Item itemInfo = Item.builder()
+            .itemName("나이키 에어포스")
+            .modelNumber("NK22035")
+            .category1("신발")
+            .category2("스니커즈")
+            .releaseDate(LocalDateTime.now())
+            .representativeColor("Black")
+            .releasedPrice(10000)
+            .build();
+
+
+    ItemImg itemImgInfo = ItemImg.builder()
+            .item(itemInfo)
+            .imgUrl("test")
+            .imgName("test")
+            .originName("test_origin")
+            .isRepresentativeImg(true)
+            .build();
+
+    @BeforeEach
+    void setUp() {
+        itemMapper.save(itemInfo);
+    }
+
+    @Test
+    void save() {
+        assertThat(itemImgMapper.save(itemImgInfo)).isEqualTo(1);
+    }
+
+    @Test
+    void findImagesByItemId() {
+        itemImgMapper.save(itemImgInfo);
+        assertThat(itemImgMapper.findImagesByItemId(itemInfo.getId()).get(0)).isEqualTo(itemImgInfo);
+    }
+
+    @Test
+    void delete() {
+        itemImgMapper.save(itemImgInfo);
+        itemImgMapper.delete(itemImgInfo.getId());
+        assertThat(itemImgMapper.findImagesByItemId(itemInfo.getId())).hasSize(0);
+    }
+}
---
+++ src/test/java/com/flab/fkream/itemImg/ItemImgServiceTest.java
@@ -0,0 +1,63 @@
+package com.flab.fkream.itemImg;
+
+import com.flab.fkream.item.Item;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ItemImgServiceTest {
+
+
+    @Mock
+    ItemImgMapper itemImgMapper;
+    @InjectMocks
+    ItemImgService itemImgService;
+
+
+    Item itemInfo = Item.builder()
+            .itemName("나이키 에어포스")
+            .modelNumber("NK22035")
+            .category1("신발")
+            .category2("스니커즈")
+            .releaseDate(LocalDateTime.now())
+            .representativeColor("Black")
+            .releasedPrice(10000)
+            .build();
+
+
+    ItemImg itemImgInfo = ItemImg.builder()
+            .item(itemInfo)
+            .imgUrl("test")
+            .imgName("test")
+            .originName("test_origin")
+            .isRepresentativeImg(true)
+            .build();
+
+    @Test
+    void addItemImg() {
+        given(itemImgMapper.save(itemImgInfo)).willReturn(1);
+        itemImgService.addItemImg(itemImgInfo);
+    }
+
+    @Test
+    void findImagesByItemId() {
+        given(itemImgMapper.findImagesByItemId(1L)).willReturn(List.of());
+        itemImgService.findImagesByItemId(1L);
+    }
+
+    @Test
+    void delete() {
+        given(itemImgMapper.delete(1L)).willReturn(1);
+        itemImgService.delete(1L);
+    }
+}
