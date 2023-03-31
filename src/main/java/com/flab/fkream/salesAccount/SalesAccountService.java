3c355a68178bd6526dbc91ff09b88189f3138504
Chanhook <cksgur97@gmail.com>
2023-03-31 12:02:44 +0900

    SalesAccountService 기능, 테스트 추가

--- src/main/java/com/flab/fkream/salesAccount/SalesAccountService.java
+++ src/main/java/com/flab/fkream/salesAccount/SalesAccountService.java
@@ -1,8 +1,35 @@
 package com.flab.fkream.salesAccount;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-@Service
-public class SalesAccountService {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class SalesAccountService {
+	private final SalesAccountMapper salesAccountMapper;
+
+	public int save(SalesAccount salesAccount) {
+		return salesAccountMapper.save(salesAccount);
+	}
+
+	public SalesAccount findById(Long id) {
+		return salesAccountMapper.findById(id);
+	}
+
+	public List<SalesAccount> findAll() {
+		return salesAccountMapper.findAll();
+	}
+
+	public int update(SalesAccount salesAccount) {
+		return salesAccountMapper.update(salesAccount);
+	}
+
+	public int deleteById(Long id) {
+		return salesAccountMapper.deleteById(id);
+	}
 }
---
+++ src/test/java/com/flab/fkream/salesAccount/SalesAccountServiceTest.java
@@ -0,0 +1,122 @@
+package com.flab.fkream.salesAccount;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.flab.fkream.users.Users;
+
+@ExtendWith(MockitoExtension.class)
+class SalesAccountServiceTest {
+	@Mock
+	SalesAccountMapper salesAccountMapper;
+
+	@InjectMocks
+	SalesAccountService salesAccountService;
+
+	Users users = Users.builder()
+		.email("test@test.com")
+		.password("testpassword")
+		.fourteenAgreement(true)
+		.adAgreement(true)
+		.personalAuthentication(true)
+		.gender("Male")
+		.phoneNumber("01012345678")
+		.name("testuser")
+		.build();
+
+	SalesAccount salesAccount = SalesAccount.builder()
+		.users(users)
+		.bankName("Test Bank")
+		.accountNumber("123-456-7890")
+		.accountHolder("Test User")
+		.build();
+
+	@Test
+	void save() {
+		//given
+		given(salesAccountMapper.save(salesAccount)).willReturn(1);
+
+		//when
+		int result = salesAccountService.save(salesAccount);
+
+		//then
+		//mock 객체에서 save() 메서드가 호출되었는지 검증
+		then(salesAccountMapper).should().save(ArgumentMatchers.any(SalesAccount.class));
+		Assertions.assertThat(result).isEqualTo(1);
+	}
+
+	@Test
+	void findById() {
+		//given
+		given(salesAccountMapper.findById(anyLong())).willReturn(salesAccount);
+
+		//when
+		SalesAccount result = salesAccountService.findById(1L);
+
+		//then
+		then(salesAccountMapper).should().findById(ArgumentMatchers.anyLong());
+		Assertions.assertThat(result).isEqualTo(salesAccount);
+	}
+
+	@Test
+	void findAll() {
+		//given
+		List<SalesAccount> salesAccounts = new ArrayList<>();
+		salesAccounts.add(salesAccount);
+		given(salesAccountMapper.findAll()).willReturn(salesAccounts);
+
+		//when
+		List<SalesAccount> result = salesAccountService.findAll();
+
+		//then
+		then(salesAccountMapper).should().findAll();
+		Assertions.assertThat(result).hasSize(1);
+		Assertions.assertThat(result.get(0)).isEqualTo(salesAccount);
+	}
+
+	@Test
+	void update() {
+		//given
+		SalesAccount updatedSalesAccount = SalesAccount.builder()
+			.id(1L)
+			.users(users)
+			.bankName("Test Bank2")
+			.accountNumber("123-456-7890")
+			.accountHolder("Test User")
+			.createdAt(LocalDateTime.now())
+			.build();
+		given(salesAccountMapper.update(any(SalesAccount.class))).willReturn(1);
+
+		//when
+		int result = salesAccountService.update(updatedSalesAccount);
+
+		//then
+		then(salesAccountMapper).should().update(ArgumentMatchers.any(SalesAccount.class));
+		Assertions.assertThat(result).isEqualTo(1);
+	}
+
+	@Test
+	void deleteById() {
+		//given
+		given(salesAccountMapper.deleteById(anyLong())).willReturn(1);
+
+		//when
+		int result = salesAccountService.deleteById(1L);
+
+		//then
+		then(salesAccountMapper).should().deleteById(ArgumentMatchers.anyLong());
+		Assertions.assertThat(result).isEqualTo(1);
+	}
+}
