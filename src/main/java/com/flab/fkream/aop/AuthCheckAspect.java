7499dc1593d62cbd53d1aaaffbf376663d7d8c12
Chanhook <cksgur97@gmail.com>
2023-04-01 23:23:54 +0900

    예외 던지기 변경
    - 바뀐 명칭으로 던지는 예외 변경
    - LoginFailureException 내부에서 default값으로 가지고 있게 변경
    
    참고: #25

--- src/main/java/com/flab/fkream/aop/AuthCheckAspect.java
+++ src/main/java/com/flab/fkream/aop/AuthCheckAspect.java
@@ -1,41 +1,36 @@
 package com.flab.fkream.aop;
 
 
-import com.flab.fkream.error.exception.NoLoginException;
+import com.flab.fkream.error.exception.LoginFailureException;
 import com.flab.fkream.utils.SessionUtil;
+import javax.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpSession;
 
 @Aspect
 @Component
 @Log4j2
 public class AuthCheckAspect {
 
-    @Before("@annotation(com.flab.fkream.aop.LoginCheck) && @annotation(loginCheck)")
-    public void loginCheck(JoinPoint joinPoint, LoginCheck loginCheck) throws Throwable{
-        log.debug("aop - login check");
+  @Before("@annotation(com.flab.fkream.aop.LoginCheck) && @annotation(loginCheck)")
+  public void loginCheck(JoinPoint joinPoint, LoginCheck loginCheck) throws Throwable {
+    log.debug("aop - login check");
 
-        HttpSession session =((ServletRequestAttributes)(RequestContextHolder.currentRequestAttributes())).getRequest().getSession();
+    HttpSession session = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest()
+        .getSession();
 
-        if (LoginCheck.UserType.USER.equals(loginCheck.type())){
-            Long loginUserId = SessionUtil.getLoginUserId(session);
-            if (loginUserId==null){
-                throw new NoLoginException("로그인이 필요합니다.");
-            }
-        }
-
-
+    if (LoginCheck.UserType.USER.equals(loginCheck.type())) {
+      Long loginUserId = SessionUtil.getLoginUserId(session);
+      if (loginUserId == null) {
+        throw new LoginFailureException();
+      }
 
     }
-
+  }
 
 }
