773bea7e60838afb76a52358add5e15480e9e356
JooDukHan <bfinecpa1@naver.com>
2023-04-01 20:40:41 +0900

    user클래스 단위 테스트 구현
    참고: #25

---
+++ src/test/java/com/flab/fkream/user/UserMapperTest.java
@@ -0,0 +1,42 @@
+package com.flab.fkream.user;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserMapperTest {
+
+    @Autowired
+    UserMapper userMapper;
+
+    User user = User.builder()
+        .email("test1")
+        .password("000")
+        .gender("aa")
+        .name("test")
+        .adAgreement(true)
+        .fourteenAgreement(true)
+        .personalAuthentication(true)
+        .phoneNumber("010")
+        .build();
+
+    @Test
+    void save() {
+        userMapper.save(user);
+        assertThat(user.getId()).isNotNull();
+    }
+
+    @Test
+    void findByEmail() {
+        userMapper.save(user);
+        User userFound = userMapper.findByEmail(user.getEmail());
+        assertThat(user).isEqualTo(userFound);
+    }
+}
