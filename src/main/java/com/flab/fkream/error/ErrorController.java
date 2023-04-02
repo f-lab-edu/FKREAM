411507efca4c1799e1d6dbb4e729f8497f9eb598
JooDukHan <bfinecpa1@naver.com>
2023-04-03 00:16:39 +0900

    예외 메세지 구현 및 처리
    참고: #14

--- src/main/java/com/flab/fkream/error/ErrorController.java
+++ src/main/java/com/flab/fkream/error/ErrorController.java
@@ -5,6 +5,7 @@ import com.flab.fkream.error.exception.DuplicateEmailException;
 import com.flab.fkream.error.exception.MapperException;
 import com.flab.fkream.error.exception.NoLoginException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,21 +14,8 @@ import org.springframework.web.client.HttpStatusCodeException;
 @RestControllerAdvice
 public class ErrorController {
 
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(DuplicateEmailException.class)
-    public ErrorMsg handleDuplicateEmailException(DuplicateEmailException e){
-        return new ErrorMsg(e.getMessage(), e.getClass().getSimpleName());
-    }
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(NoLoginException.class)
-    public ErrorMsg handleNoLogin(NoLoginException e){
-        return new ErrorMsg(e.getMessage(), e.getClass().getSimpleName());
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(MapperException.class)
-    public ErrorMsg handleMapperException(MapperException e){
-        return new ErrorMsg(e.getMessage(), e.getClass().getSimpleName());
+    public ResponseEntity handleMapperException(MapperException e){
+        return ErrorMsg.toResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, e);
     }
 }
