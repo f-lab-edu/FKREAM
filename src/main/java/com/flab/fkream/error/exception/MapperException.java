e4bb87f795dfdb95d2717b6945a1eb69c8b0abca
JooDukHan <bfinecpa1@naver.com>
2023-04-03 00:29:43 +0900

    mapper 관련 예외 클래스 구현
    매퍼에서 예외가 발생시 예외를 처리하고 해당하는 예외를 일으킨다.
    
    참고: #14

--- src/main/java/com/flab/fkream/error/exception/MapperException.java
+++ src/main/java/com/flab/fkream/error/exception/MapperException.java
@@ -2,7 +2,9 @@ package com.flab.fkream.error.exception;
 
 public class MapperException extends RuntimeException{
 
-    public MapperException(String msg){
-        super(msg);
+    private static final String MESSAGE = "서버 에러입니다. 다시 시도해주세요.";
+
+    public MapperException(Exception e){
+        super(MESSAGE, e);
     }
 }
