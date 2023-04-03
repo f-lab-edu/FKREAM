a1d3169df8bc4eb77ba9eff587bb6a8135c72686
JooDukHan <bfinecpa1@naver.com>
2023-04-03 21:42:02 +0900

    예외 처리
    참고: #20

--- src/main/java/com/flab/fkream/error/ErrorMsg.java
+++ src/main/java/com/flab/fkream/error/ErrorMsg.java
@@ -1,18 +1,36 @@
 package com.flab.fkream.error;
 
+import com.flab.fkream.utils.HttpRequestUtils;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.RequestContextHolder;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;import java.util.List;
 
 @Getter
-@NoArgsConstructor
+@SuperBuilder
 public class ErrorMsg {
 
-    private String msg;
-    private String errorCode;
+    private final LocalDateTime timestamp = LocalDateTime.now();
 
+    private final String error;
+    private final int code;
+    private final String message;
+    private final String path;
 
-    public ErrorMsg(String msg, String errorCode) {
-        this.msg = msg;
-        this.errorCode = errorCode;
+    public static ResponseEntity<ErrorMsg> toResponseEntity(HttpStatus httpStatus, Exception e) {
+        return ResponseEntity.status(httpStatus)
+            .body(
+                ErrorMsg.builder()
+                    .error(httpStatus.name())
+                    .code(httpStatus.value())
+                    .message(e.getMessage())
+                    .path(HttpRequestUtils.getRequest().getRequestURI())
+                    .build());
     }
 }
