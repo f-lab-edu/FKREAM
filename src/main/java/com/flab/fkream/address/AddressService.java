2ac067c12dffc5a7e9e97091c3c7db5bbe0a521f
JooDukHan <bfinecpa1@naver.com>
2023-04-03 21:44:24 +0900

    address 관련 controller, service, mapper 구현
    참고: #20

--- src/main/java/com/flab/fkream/address/AddressService.java
+++ src/main/java/com/flab/fkream/address/AddressService.java
@@ -1,7 +1,71 @@
 package com.flab.fkream.address;
 
+import com.flab.fkream.error.exception.MapperException;
+import com.flab.fkream.error.exception.NoDataFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
+@RequiredArgsConstructor
 public class AddressService {
+
+    private final AddressMapper addressMapper;
+
+    public void addAddress(Address address) {
+        try {
+            addressMapper.save(address);
+        } catch (DataAccessException e) {
+            log.error("[AddressService.addAddress] insert address error! addressInfo : {}",
+                address);
+            throw new MapperException(e);
+        }
+    }
+
+    public Address findOne(Long id) {
+        try {
+            Address address = addressMapper.findOne(id);
+            if (address == null) {
+                throw new NoDataFoundException();
+            }
+            return address;
+        } catch (DataAccessException e) {
+            log.error("[AddressService.findOne] find address error!");
+            throw new MapperException(e);
+        }
+    }
+
+    public List<Address> findByUserId(Long userId) {
+        try {
+            List<Address> addresses = addressMapper.findByUserId(userId);
+            if(addresses.size() == 0){
+                throw new NoDataFoundException();
+            }
+            return addresses;
+        } catch (DataAccessException e) {
+            log.error("[AddressService.findOneByUserId] find address by userId error! ");
+            throw new MapperException(e);
+        }
+    }
+
+    public void update(Address addressInfo) {
+        try {
+            addressMapper.update(addressInfo);
+        } catch (DataAccessException e) {
+            log.error("[AddressService.update] update address error!");
+            throw new MapperException(e);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            addressMapper.delete(id);
+        } catch (DataAccessException e) {
+            log.error("[AddressService.delete] delete address error! ");
+            throw new MapperException(e);
+        }
+    }
 }
