66c7f1a37910827388541cc474a09be0693c2035
JooDukHan <bfinecpa1@naver.com>
2023-04-03 21:44:57 +0900

    notnull 추가
    참고: #20

--- src/main/java/com/flab/fkream/address/Address.java
+++ src/main/java/com/flab/fkream/address/Address.java
@@ -1,31 +1,38 @@
 package com.flab.fkream.address;
 
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
-
-import com.flab.fkream.users.Users;
 
 @Getter
 @Builder
 @EqualsAndHashCode(of = "id")
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     private Long id;
-    private final Users users;
-    private final String name;
-    private final String phoneNumber;
-    private final String zipcode;
-    private final String detail1;
-    private final String detail2;
-
-    private final boolean defaultAddress;
-    private final LocalDateTime createdAt;
-
+    @NotNull
+    private Long userId;
+    @NotNull
+    private String name;
+    @NotNull
+    private String phoneNumber;
+    @NotNull
+    private String zipcode;
+    @NotNull
+    private String detail1;
+    @NotNull
+    private String detail2;
+    private boolean defaultAddress;
+    private LocalDateTime createdAt;
 
 
 }
