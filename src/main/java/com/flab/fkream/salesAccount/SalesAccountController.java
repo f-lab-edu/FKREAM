7546ba6151411d97efa8c8ff687f85188acffd67
Chanhook <cksgur97@gmail.com>
2023-04-03 12:48:01 +0900

    SalesAccountController Resource URL spinal case 로 변경
    참고: #24

--- src/main/java/com/flab/fkream/salesAccount/SalesAccountController.java
+++ src/main/java/com/flab/fkream/salesAccount/SalesAccountController.java
@@ -19,46 +19,47 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/salesAccounts")
+@RequestMapping("/sales-accounts")
 public class SalesAccountController {
-	private final SalesAccountService salesAccountService;
 
-	// salesAccount 추가
-	@PostMapping("")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void save(@RequestBody @NonNull SalesAccount salesAccountInfo) {
-		salesAccountService.save(salesAccountInfo);
-	}
+  private final SalesAccountService salesAccountService;
 
-	// salesAccount 리스트 조회
-	@GetMapping("")
-	@ResponseStatus(HttpStatus.OK)
-	public List<SalesAccount> findAll() {
-		return salesAccountService.findAll();
-	}
+  // salesAccount 추가
+  @PostMapping("")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void save(@RequestBody @NonNull SalesAccount salesAccountInfo) {
+    salesAccountService.save(salesAccountInfo);
+  }
 
-	// salesAccount 단건 조회
-	@GetMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public SalesAccount findOne(@PathVariable Long id) {
-		return salesAccountService.findById(id);
-	}
+  // salesAccount 리스트 조회
+  @GetMapping("")
+  @ResponseStatus(HttpStatus.OK)
+  public List<SalesAccount> findAll() {
+    return salesAccountService.findAll();
+  }
 
-	// salesAccount 수정
-	@PatchMapping("{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public void update(@RequestBody SalesAccount salesAccountInfo) {
-		salesAccountService.update(salesAccountInfo);
-	}
+  // salesAccount 단건 조회
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public SalesAccount findOne(@PathVariable Long id) {
+    return salesAccountService.findById(id);
+  }
 
-	// salesAccount 삭제
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public void delete(@PathVariable Long id) {
-		int result = salesAccountService.deleteById(id);
-		if (result == 0) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 id의 sales account가 존재하지 않습니다.");
-		}
-	}
+  // salesAccount 수정
+  @PatchMapping("{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public void update(@RequestBody SalesAccount salesAccountInfo) {
+    salesAccountService.update(salesAccountInfo);
+  }
+
+  // salesAccount 삭제
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public void delete(@PathVariable Long id) {
+    int result = salesAccountService.deleteById(id);
+    if (result == 0) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 id의 sales account가 존재하지 않습니다.");
+    }
+  }
 
 }
