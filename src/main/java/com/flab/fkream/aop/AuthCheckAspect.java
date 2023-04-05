4b2c714e36828b3e44bdd78eed8bdf9ba07aa91e
JooDukHan <bfinecpa1@naver.com>
2023-04-05 11:28:02 +0900

    세션 가져오는 방식 변경 및 예외 변경
    참고: #25

--- src/main/java/com/flab/fkream/aop/AuthCheckAspect.java
+++ src/main/java/com/flab/fkream/aop/AuthCheckAspect.java
@@ -1,9 +1,9 @@
 package com.flab.fkream.aop;
 
 
-import com.flab.fkream.error.exception.LoginFailureException;
+import com.flab.fkream.error.exception.LoginRequiredException;
+import com.flab.fkream.utils.HttpRequestUtils;
 import com.flab.fkream.utils.SessionUtil;
-import javax.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,25 +12,24 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpSession;
+
 @Aspect
 @Component
 @Log4j2
 public class AuthCheckAspect {
 
-  @Before("@annotation(com.flab.fkream.aop.LoginCheck) && @annotation(loginCheck)")
-  public void loginCheck(JoinPoint joinPoint, LoginCheck loginCheck) throws Throwable {
-    log.debug("aop - login check");
+    @Before("@annotation(com.flab.fkream.aop.LoginCheck) && @annotation(loginCheck)")
+    public void loginCheck(JoinPoint joinPoint, LoginCheck loginCheck) throws Throwable {
+        log.debug("aop - login check");
 
-    HttpSession session = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest()
-        .getSession();
+        HttpSession session = HttpRequestUtils.getRequest().getSession();
 
-    if (LoginCheck.UserType.USER.equals(loginCheck.type())) {
-      Long loginUserId = SessionUtil.getLoginUserId(session);
-      if (loginUserId == null) {
-        throw new LoginFailureException();
-      }
-
+        if (LoginCheck.UserType.USER.equals(loginCheck.type())) {
+            Long loginUserId = SessionUtil.getLoginUserId(session);
+            if (loginUserId == null) {
+                throw new LoginRequiredException();
+            }
+        }
     }
-  }
-
 }
