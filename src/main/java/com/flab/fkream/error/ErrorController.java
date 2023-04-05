76f692764eab0b935f3624362f6e6b4f346e3915
JooDukHan <bfinecpa1@naver.com>
2023-04-05 14:33:57 +0900

    DataAccessException을 핸들하는 것으로 변경
    mapperException -> DataAccessException
    
    참고: #14

--- src/main/java/com/flab/fkream/error/ErrorController.java
+++ src/main/java/com/flab/fkream/error/ErrorController.java
@@ -1,21 +1,17 @@
 package com.flab.fkream.error;
 
 
-import com.flab.fkream.error.exception.DuplicateEmailException;
-import com.flab.fkream.error.exception.MapperException;
-import com.flab.fkream.error.exception.NoLoginException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.client.HttpStatusCodeException;
 
 @RestControllerAdvice
 public class ErrorController {
 
-    @ExceptionHandler(MapperException.class)
-    public ResponseEntity handleMapperException(MapperException e){
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity handleMapperException(DataAccessException e) {
         return ErrorMsg.toResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, e);
     }
 }
