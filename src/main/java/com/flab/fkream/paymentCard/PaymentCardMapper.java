d0eb49abedac156f5bf79995b8bb2ef0ae51da21
JooDukHan <bfinecpa1@naver.com>
2023-04-09 15:40:08 +0900

    paymentCard controller, service, mapper 구현
    참고: #28

--- src/main/java/com/flab/fkream/paymentCard/PaymentCardMapper.java
+++ src/main/java/com/flab/fkream/paymentCard/PaymentCardMapper.java
@@ -1,9 +1,16 @@
 package com.flab.fkream.paymentCard;
 
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PaymentCardMapper {
 
-    void save(PaymentCard paymentCard);
+    int save(PaymentCard paymentCard);
+
+    List<PaymentCard> findByUserId(Long id);
+
+    PaymentCard findById(Long id);
+
+    int delete(Long id);
 }
--- src/main/java/com/flab/fkream/paymentCard/PaymentCardService.java
+++ src/main/java/com/flab/fkream/paymentCard/PaymentCardService.java
@@ -1,7 +1,32 @@
 package com.flab.fkream.paymentCard;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
+@RequiredArgsConstructor
 public class PaymentCardService {
+
+    private final PaymentCardMapper paymentCardMapper;
+
+    public void save(PaymentCard paymentCard) {
+        paymentCard.setCreatedAtToNow();
+        paymentCard.encryptPassword();
+        paymentCardMapper.save(paymentCard);
+    }
+
+    public List<PaymentCard> findByUserId(Long id) {
+        return paymentCardMapper.findByUserId(id);
+    }
+
+    public PaymentCard findById(Long id) {
+        return paymentCardMapper.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        paymentCardMapper.delete(id);
+    }
 }
