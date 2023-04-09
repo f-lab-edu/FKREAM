c77b499d0cda17d77ce0d7afd88d1853e9090815
JooDukHan <bfinecpa1@naver.com>
2023-04-09 15:37:02 +0900

    암호화 기능, 생성 시간 설정 기능, notNull
    참고: #28

--- src/main/java/com/flab/fkream/paymentCard/PaymentCard.java
+++ src/main/java/com/flab/fkream/paymentCard/PaymentCard.java
@@ -1,9 +1,13 @@
 package com.flab.fkream.paymentCard;
 
 import com.flab.fkream.user.User;
+import com.flab.fkream.utils.SHA256Util;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
@@ -12,15 +16,29 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 @EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class PaymentCard {
 
     private Long id;
-    private final User user;
-    private final String cardCompany;
-    private final String cardNumber;
-    private final LocalDate  expiration;
-    private final String cardPw;
-    private final LocalDateTime createdAt;
+    @NotNull
+    private Long userId;
+    @NotNull
+    private String cardCompany;
+    @NotNull
+    private String cardNumber;
+    @NotNull
+    private String expiration;
+    @NotNull
+    private String cardPw;
+    private LocalDateTime createdAt;
 
+    public void setCreatedAtToNow() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void encryptPassword() {
+        cardPw=SHA256Util.encrypt(cardPw);
+    }
 }
