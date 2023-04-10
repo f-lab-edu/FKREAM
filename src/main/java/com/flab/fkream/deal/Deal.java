9fe69058e5da8efea56593d972b0a087692ebc6d
JooDukHan <bfinecpa1@naver.com>
2023-04-10 20:45:58 +0900

    status 설정, createdAt 설정, kindOfDeal 설정 기능 구현
    참고: #29

--- src/main/java/com/flab/fkream/deal/Deal.java
+++ src/main/java/com/flab/fkream/deal/Deal.java
@@ -2,9 +2,14 @@ package com.flab.fkream.deal;
 
 import com.flab.fkream.item.Item;
 import com.flab.fkream.user.User;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
@@ -13,19 +18,49 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Deal {
 
     private Long id;
-    private final Item item;
-    private final KindOfDeal kindOfDeal;
-    private final User user;
-    private final int price;
-    private final String size;
-    private final LocalDate period;
-    private final boolean utilizationPolicy;
-    private final boolean salesCondition;
-    private final Status status;
-    private final LocalDateTime createAt;
-    private final LocalDateTime modifiedAt;
+    @NotNull
+    private Item item;
+    @NotNull
+    private KindOfDeal kindOfDeal;
+    @NotNull
+    private Long userId;
+    @NotNull
+    private int price;
+    @NotNull
+    private String size;
+    @NotNull
+    private LocalDate period;
+    @AssertTrue
+    private boolean utilizationPolicy;
+    @AssertTrue
+    private boolean salesCondition;
+    private Status status;
+
+    @Setter
+    private Long otherId;
+
+    private LocalDateTime createdAt;
+
+
+    public void setKindOfDealToSale(){
+        kindOfDeal = KindOfDeal.SALE;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setKindOfDealToPurchase() {
+        kindOfDeal = KindOfDeal.PURCHASE;
+    }
+
+    public void setCreatedAtToNow(){
+        createdAt = LocalDateTime.now();
+    }
 }
