f58440f1ba2668ce45060ac4638837487d3ee77e
JooDukHan <bfinecpa1@naver.com>
2023-04-16 21:29:15 +0900

    아이템 등록시 Trie 트리 아이템 명칭 저장
    참고: #38

--- src/main/java/com/flab/fkream/item/ItemService.java
+++ src/main/java/com/flab/fkream/item/ItemService.java
@@ -3,6 +3,7 @@ package com.flab.fkream.item;
 import com.flab.fkream.brand.Brand;
 import com.flab.fkream.brand.BrandService;
 import com.flab.fkream.error.exception.NoDataFoundException;
+import com.flab.fkream.search.Trie;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -18,9 +19,12 @@ public class ItemService {
     private final ItemMapper itemMapper;
     private final BrandService brandService;
 
+    private final Trie trie;
+
     public void addItem(Item itemInfo) {
         itemInfo.setCreatedAtToNow();
         itemMapper.save(itemInfo);
+        trie.insert(itemInfo.getItemName());
     }
 
     public Item findOne(Long itemId) {
