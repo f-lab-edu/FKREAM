d667cd69b78952f08f581962292fd6409d076e97
JooDukHan <bfinecpa1@naver.com>
2023-04-23 16:42:45 +0900

    리스팅 캐싱 기능 구현
    참고: #15, #42, #43, #44, #45, #46

--- src/main/java/com/flab/fkream/listing/ListingMapper.java
+++ src/main/java/com/flab/fkream/listing/ListingMapper.java
@@ -4,20 +4,27 @@ import com.flab.fkream.search.SearchItemDto;
 import java.time.LocalDate;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.cache.annotation.Cacheable;
 
 @Mapper
 public interface ListingMapper {
 
-    List<SearchItemDto> generateRecommendedItemsListForMen(LocalDate now, LocalDate lastMonth);
+    @Cacheable(cacheNames = "Listing", key = "#root.methodName")
+    List<SearchItemDto> generateRecommendedItemsForMen(LocalDate now, LocalDate lastMonth);
 
-    List<SearchItemDto> generateRecommendedItemsListForWomen(LocalDate now, LocalDate lastMonth);
+    @Cacheable(cacheNames = "Listing", key = "#root.methodName")
+    List<SearchItemDto> generateRecommendedItemsForWomen(LocalDate now, LocalDate lastMonth);
 
+    @Cacheable(cacheNames = "Listing", key = "#root.methodName")
     List<SearchItemDto> generateItemsBelowReleasedPrice(LocalDate now, LocalDate lastMonth);
 
+    @Cacheable(cacheNames = "Listing", key = "#root.methodName")
     List<SearchItemDto> generatePopularLuxuryItems(LocalDate now, LocalDate lastMonth);
 
+    @Cacheable(cacheNames = "Listing", key = "#root.methodName")
     List<SearchItemDto> generateMostPopularItems();
 
+    @Cacheable(cacheNames = "Listing", key = "#root.methodName")
     List<SearchItemDto> generatePopularSneakers(LocalDate now, LocalDate lastMonth,
         LocalDate twoMonthAgo);
 }
