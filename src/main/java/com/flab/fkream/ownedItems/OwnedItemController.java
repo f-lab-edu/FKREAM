1fdaa8e644e0ed0cdf99a1ebf1d647e20d94886c
Chanhook <cksgur97@gmail.com>
2023-04-25 09:24:01 +0900

    보유상품 Controller 기능 추가
    - 저장
    - 단건 조회
    - 유저 별 조회
    - 수정
    - 삭제

--- src/main/java/com/flab/fkream/ownedItems/OwnedItemController.java
+++ src/main/java/com/flab/fkream/ownedItems/OwnedItemController.java
@@ -1,34 +1,71 @@
 package com.flab.fkream.ownedItems;
 
+import com.flab.fkream.error.exception.ForbiddenException;
+import com.flab.fkream.utils.SessionUtil;
 import java.util.List;
-
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class OwnedItemController {
-	private final OwnedItemService ownedItemService;
 
-	@PostMapping("/ownedItem")
-	public HttpStatus addOwnedItem(@RequestBody OwnedItem ownedItemInfo) {
-		ownedItemService.addOwnedItem(ownedItemInfo);
-		return HttpStatus.CREATED;
-	}
+    private final OwnedItemService ownedItemService;
 
-	@GetMapping("/ownedItems")
-	public List<OwnedItem> findAll() {
-		return ownedItemService.findAll();
-	}
+    @PostMapping("/owned-items")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addOwnedItem(@Valid @RequestBody @NotNull OwnedItem ownedItemInfo) {
+        ownedItemService.save(ownedItemInfo);
+    }
 
-	@GetMapping("/ownedItem/{id}")
-	public OwnedItem findOne(@PathVariable Long id) {
-		return ownedItemService.findOne(id);
-	}
+    @GetMapping("/owned-item/{id}")
+    public ResponseEntity<OwnedItem> findOne(@PathVariable Long id) {
+        OwnedItem ownedItem = ownedItemService.findOne(id);
+        if (ownedItem == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ownedItem);
+    }
+
+    @GetMapping("/owned-items/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<OwnedItem> findAllByUserId(@PathVariable Long id) {
+        Long loggedInUserId = SessionUtil.getLoginUserId();
+        if (!id.equals(loggedInUserId)) {
+            throw new ForbiddenException();
+        }
+        return ownedItemService.findAllByUserId(id);
+    }
+
+
+    @PatchMapping("/owned-items/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@Valid @RequestBody @NotNull OwnedItem ownedItemInfo) {
+        Long loggedInUserId = SessionUtil.getLoginUserId();
+        if (!ownedItemInfo.getUserId().equals(loggedInUserId)) {
+            throw new ForbiddenException();
+        }
+        ownedItemService.update(ownedItemInfo);
+    }
+
+    @DeleteMapping("/owned-items/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        Long loggedInUserId = SessionUtil.getLoginUserId();
+        if (!ownedItemService.findOne(id).getUserId().equals(loggedInUserId)) {
+            throw new ForbiddenException();
+        }
+        ownedItemService.delete(id);
+    }
 }
