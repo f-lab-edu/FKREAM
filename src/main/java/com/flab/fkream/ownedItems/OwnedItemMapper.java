eab328a8d0b4dd5a9907451abf7e27d6801bd5a0
Chanhook <cksgur97@gmail.com>
2023-04-25 09:18:51 +0900

    보유상품 Mapper 수정, 삭제 기능 추가

--- src/main/java/com/flab/fkream/ownedItems/OwnedItemMapper.java
+++ src/main/java/com/flab/fkream/ownedItems/OwnedItemMapper.java
@@ -1,14 +1,18 @@
 package com.flab.fkream.ownedItems;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OwnedItemMapper {
-	Long save(OwnedItem ownedItem);
 
-	OwnedItem findOne(Long id);
+    int save(OwnedItem ownedItem);
 
-	List<OwnedItem> findAll();
+    OwnedItem findOne(Long id);
+
+    List<OwnedItem> findAllByUserId(Long userId);
+
+    int update(OwnedItem ownedItem);
+
+    int delete(Long id);
 }
