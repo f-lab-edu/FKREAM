c66d9d18e21544c941d7a7f408e008616da9820d
Chanhook <cksgur97@gmail.com>
2023-04-25 09:20:55 +0900

    보유상품 Service 기능 추가
    - 저장
    - 단건 조회
    - 유저 별 조회
    - 수정
    - 삭제

--- src/main/java/com/flab/fkream/ownedItems/OwnedItemService.java
+++ src/main/java/com/flab/fkream/ownedItems/OwnedItemService.java
@@ -1,34 +1,52 @@
 package com.flab.fkream.ownedItems;
 
+import com.flab.fkream.error.exception.NoDataFoundException;
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class OwnedItemService {
-	private final OwnedItemMapper ownedItemMapper;
 
-	@Transactional(rollbackFor = RuntimeException.class)
-	public Long addOwnedItem(OwnedItem ownedItemInfo) {
-		Long ownedItemId = ownedItemMapper.save(ownedItemInfo);
-		if (ownedItemId == null) {
-			log.error("insert ownedItem error! ownedInfo : {}", ownedItemInfo);
-			throw new NullPointerException("insert ownedItem error!" + ownedItemInfo);
-		}
-		return ownedItemId;
-	}
+    private final OwnedItemMapper ownedItemMapper;
 
-	public OwnedItem findOne(Long ownedItemId) {
-		return ownedItemMapper.findOne(ownedItemId);
-	}
+    public int save(OwnedItem ownedItemInfo) {
+        return ownedItemMapper.save(ownedItemInfo);
+    }
 
-	public List<OwnedItem> findAll() {
-		return ownedItemMapper.findAll();
-	}
+    public OwnedItem findOne(Long ownedItemId) {
+        OwnedItem ownedItem = ownedItemMapper.findOne(ownedItemId);
+        if (ownedItem == null) {
+            log.error("OwnedItem with id {} not found.", ownedItemId);
+            throw new NoDataFoundException();
+        }
+        return ownedItem;
+    }
+
+    public List<OwnedItem> findAllByUserId(Long userId) {
+        return ownedItemMapper.findAllByUserId(userId);
+    }
+
+    public int update(OwnedItem ownedItem) {
+        int result = ownedItemMapper.update(ownedItem);
+        if (result == 0) {
+            log.error("Failed to update OwnedItem with id {}.", ownedItem.getId());
+            throw new NoDataFoundException();
+        }
+        return result;
+    }
+
+    public int delete(Long id) {
+        int result = ownedItemMapper.delete(id);
+        if (result == 0) {
+            log.error("Failed to delete OwnedItem with id {}.", id);
+            throw new NoDataFoundException();
+        }
+        return result;
+    }
+
+
 }
