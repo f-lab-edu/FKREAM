2af852afbfa7eb25cccc22e1bd713bbab9326d08
JooDukHan <bfinecpa1@naver.com>
2023-04-27 08:46:43 +0900

    알림 제목, 내용 클래스 구현
    참고: #57

---
+++ src/main/java/com/flab/fkream/notification/TitleInfo.java
@@ -0,0 +1,12 @@
+package com.flab.fkream.notification;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class TitleInfo {
+
+    private String title;
+    private String context;
+}
