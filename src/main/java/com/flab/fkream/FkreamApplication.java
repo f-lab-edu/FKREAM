0f32e487fa7002c7e9c3b7d041849f36f5b67b40
JooDukHan <bfinecpa1@naver.com>
2023-05-07 00:46:58 +0900

    redis 세션 설정
    참고:

--- src/main/java/com/flab/fkream/FkreamApplication.java
+++ src/main/java/com/flab/fkream/FkreamApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableRedisHttpSession
 @EnableCaching
 public class FkreamApplication {
 
