bb85cec796582173cf4e7fe6f9af26b458df4df2
Chanhook <cksgur97@gmail.com>
2023-06-28 22:33:41 +0900

    카프카 토픽 상수 클래스로 변경
    - Enum 클래스에서 상수 클래스로 변경

--- src/main/java/com/flab/fkream/kafka/KafkaTopic.java
+++ src/main/java/com/flab/fkream/kafka/KafkaTopic.java
@@ -2,5 +2,8 @@ package com.flab.fkream.kafka;
 
 public class KafkaTopic {
 
-    search_log, complete_deal_price
+    public static final String ITEM_TOPIC = "item";
+    public static final String SEARCH_LOG = "search_log";
+    public static final String DEAL = "deal";
+    public static final String ITEM_PURCHASE_PRICE = "item_purchase_price";
 }
