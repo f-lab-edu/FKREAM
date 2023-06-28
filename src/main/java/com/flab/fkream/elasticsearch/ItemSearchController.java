87c327f8be63c561faa37e768ffee131308e4fe5
Chanhook <cksgur97@gmail.com>
2023-06-28 22:42:36 +0900

    엘라스틱서치 아이템 검색 컨트롤러 추가

--- src/main/java/com/flab/fkream/elasticsearch/ItemSearchController.java
+++ src/main/java/com/flab/fkream/elasticsearch/ItemSearchController.java
@@ -1,9 +1,16 @@
 package com.flab.fkream.elasticsearch;
 
+import com.flab.fkream.deal.Deal;
+import com.flab.fkream.item.Item;
+import com.flab.fkream.itemSizePrice.ItemSizePrice;
+import com.flab.fkream.kafka.KafkaMessageSender;
+import com.flab.fkream.kafka.KafkaTopic;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +21,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemSearchController {
 
     private final ItemSearchService itemSearchService;
+    private final KafkaMessageSender messageSender;
+
+
+    @GetMapping("/estest1")
+    public void test1() {
+        ItemSizePrice itemSizePrice = new ItemSizePrice(1L, 1L, "M", 12000, 6000,
+            LocalDateTime.now());
+        messageSender.send(KafkaTopic.ITEM_PURCHASE_PRICE, itemSizePrice);
+    }
+
+
+    @GetMapping("/estest2")
+    public void test2() {
+
+        Item item1 = Item.builder()
+            .id(1L).build();
+        ItemSizePrice item1SizePrice = new ItemSizePrice(1L, item1.getId(), "S", 10000, 6000,
+            LocalDateTime.now());
+
+        Deal deal = Deal.builder()
+            .id(1L).price(20000).item(item1).size(item1SizePrice.getSize()).build();
+
+        messageSender.send(KafkaTopic.DEAL, deal);
+    }
 
     @PostMapping("/item_document")
     @ResponseStatus(HttpStatus.CREATED)
