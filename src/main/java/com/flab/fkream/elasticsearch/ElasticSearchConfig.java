eb325a51aa690e76eecf5745b3a7f5fba18822dd
Chanhook <cksgur97@gmail.com>
2023-07-02 10:08:23 +0900

    ElasticSearchConfig 변경

--- src/main/java/com/flab/fkream/elasticsearch/ElasticSearchConfig.java
+++ src/main/java/com/flab/fkream/elasticsearch/ElasticSearchConfig.java
@@ -2,13 +2,10 @@ package com.flab.fkream.elasticsearch;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
@@ -18,10 +15,17 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
     @Value("${spring.elasticsearch.uris}")
     private String server;
 
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
             .connectedTo(server)
+            .withBasicAuth(username, password)
             .build();
         return RestClients.create(clientConfiguration).rest();
     }
