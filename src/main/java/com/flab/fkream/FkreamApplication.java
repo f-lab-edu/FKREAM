cff5867f4dabfced8bb9c0e89390f1b4070f4162
JooDukHan <bfinecpa1@naver.com>
2023-10-27 17:27:16 +0900

    feat: jpa auditing 추가

--- src/main/java/com/flab/fkream/FkreamApplication.java
+++ src/main/java/com/flab/fkream/FkreamApplication.java
@@ -2,8 +2,10 @@ package com.flab.fkream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FkreamApplication {
 
     public static void main(String[] args) {
