5185bf4ff2207dd41982cb439c6f20475461abf3
Chanhook <cksgur97@gmail.com>
2023-10-28 16:09:25 +0900

    feat: Swagger Jwt 인증 기능 추가
    - Authentication 추가
    
    Resolves: #72

--- src/main/java/com/flab/fkream/config/SwaggerConfig.java
+++ src/main/java/com/flab/fkream/config/SwaggerConfig.java
@@ -23,7 +23,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
@@ -34,13 +38,36 @@ public class SwaggerConfig {
      */
     @Bean
     public Docket FkreamApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
+            .useDefaultResponseMessages(false)
+            .securityContexts(List.of(this.securityContext()))
+            .securitySchemes(List.of(this.apiKey()))
             .groupName("Fkream API")
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.flab.fkream.controller"))
             .paths(PathSelectors.any())
             .build()
             .apiInfo(this.FkreamApiInfo());
+    }
+
+    // JWT SecurityContext 구성
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+            "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    // ApiKey 정의
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 
     private ApiInfo FkreamApiInfo() {
